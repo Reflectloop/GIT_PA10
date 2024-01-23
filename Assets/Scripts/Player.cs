2b8aa9e9101e0ae22df0930624d1a209310978d3
Reflectloop <135951011+Reflectloop@users.noreply.github.com>
2024-01-23 12:31:14 +0800

    Added Finished Prototype

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+
+
+    [SerializeField] private float _velocity = 1.5f;
+    public int Score = 0;
+    public Text scoreText;
+    private Rigidbody _rb;
     private Animation thisAnimation;
 
     void Start()
     {
+        _rb = GetComponent<Rigidbody>();
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
     }
@@ -15,6 +24,32 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            _rb.velocity = Vector2.up * _velocity;
             thisAnimation.Play();
+
+
+        }
+            
+
+
     }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == ("Score"))
+        {
+            Score += 1;
+            scoreText.text = "Score: " + Score.ToString();
+        }
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+
 }
